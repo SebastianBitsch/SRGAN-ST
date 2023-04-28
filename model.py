0096e39d9fa9249a9bbf5d06cdb92c7922b59bf0
SebastianBitsch <sebastianbitsch@protonmail.com>
2023-04-28 10:03:29 +0200

    refactored model

--- model.py
+++ model.py
@@ -17,10 +17,6 @@ from typing import Any
 import torch
 from torch import Tensor
 from torch import nn
-from torch.nn import functional as F_torch
-from torchvision import models
-from torchvision import transforms
-from torchvision.models.feature_extraction import create_feature_extractor
 
 __all__ = [
     "SRResNet", "Discriminator",
@@ -184,54 +180,6 @@ class _UpsampleBlock(nn.Module):
         return out
 
 
-class ContentLoss(nn.Module):
-    """Constructs a content loss function based on the VGG19 network.
-    Using high-level feature mapping layers from the latter layers will focus more on the texture content of the image.
-
-    Paper reference list:
-        -`Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network <https://arxiv.org/pdf/1609.04802.pdf>` paper.
-        -`ESRGAN: Enhanced Super-Resolution Generative Adversarial Networks                    <https://arxiv.org/pdf/1809.00219.pdf>` paper.
-        -`Perceptual Extreme Super Resolution Network with Receptive Field Block               <https://arxiv.org/pdf/2005.12597.pdf>` paper.
-
-     """
-
-    def __init__(
-            self,
-            feature_model_extractor_node: str,
-            feature_model_normalize_mean: list,
-            feature_model_normalize_std: list
-    ) -> None:
-        super(ContentLoss, self).__init__()
-        # Get the name of the specified feature extraction node
-        self.feature_model_extractor_node = feature_model_extractor_node
-        # Load the VGG19 model trained on the ImageNet dataset.
-        model = models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1)
-        # Extract the thirty-sixth layer output in the VGG19 model as the content loss.
-        self.feature_extractor = create_feature_extractor(model, [feature_model_extractor_node])
-        # set to validation mode
-        self.feature_extractor.eval()
-
-        # The preprocessing method of the input data. 
-        # This is the VGG model preprocessing method of the ImageNet dataset.
-        self.normalize = transforms.Normalize(feature_model_normalize_mean, feature_model_normalize_std)
-
-        # Freeze model parameters.
-        for model_parameters in self.feature_extractor.parameters():
-            model_parameters.requires_grad = False
-
-    def forward(self, sr_tensor: Tensor, gt_tensor: Tensor) -> Tensor:
-        # Standardized operations
-        sr_tensor = self.normalize(sr_tensor)
-        gt_tensor = self.normalize(gt_tensor)
-
-        sr_feature = self.feature_extractor(sr_tensor)[self.feature_model_extractor_node]
-        gt_feature = self.feature_extractor(gt_tensor)[self.feature_model_extractor_node]
-
-        # Find the feature map difference between the two images
-        loss = F_torch.mse_loss(sr_feature, gt_feature)
-        
-        return loss
-
 
 def srresnet_x4(**kwargs: Any) -> SRResNet:
     model = SRResNet(upscale_factor=4, **kwargs)
@@ -244,8 +192,3 @@ def discriminator() -> Discriminator:
 
     return model
 
-
-def content_loss(**kwargs: Any) -> ContentLoss:
-    content_loss = ContentLoss(**kwargs)
-
-    return content_loss
