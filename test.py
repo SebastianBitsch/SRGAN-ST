a38ee6b51d3e0a8e901e83c092ae9c31425af59b
SebastianBitsch <sebastianbitsch@protonmail.com>
2023-05-23 15:58:19 +0200

    updated test.py

--- test.py
+++ test.py
@@ -12,7 +12,6 @@
 # limitations under the License.
 # ==============================================================================
 import os
-import argparse
 
 import cv2
 import torch
@@ -20,62 +19,45 @@ from natsort import natsorted
 
 import imgproc
 import model
-import srgan_config
 from image_quality_assessment import PSNR, SSIM
 
+from config import Config
 
-def main() -> None:
+def test(config: Config, g_path: str) -> None:
     # Initialize the super-resolution bsrgan_model
-    g_model = model.Generator(
-        in_channels=srgan_config.in_channels,
-        out_channels=srgan_config.out_channels,
-        channels=srgan_config.channels,
-        num_rcb=srgan_config.num_rcb,
-        upscale_factor=srgan_config.upscale_factor
-    )
-    g_model = g_model.to(device=srgan_config.device)
-    print(f"Successfully built generator.")
-
-    # Load the super-resolution bsrgan_model weights
-    checkpoint = torch.load(srgan_config.g_model_weights_path, map_location=lambda storage, _: storage)
-    g_model.load_state_dict(checkpoint["state_dict"])
-    print(f"Loaded generator model weights {os.path.abspath(srgan_config.g_model_weights_path)} successfully.")
+    generator = model.Generator(config).to(config.MODEL.DEVICE)
+    generator.load_state_dict(torch.load(g_path))
 
     # Create a folder of super-resolution experiment results
-    os.makedirs(srgan_config.sr_dir, exist_ok=True)
+    os.makedirs(os.path.join(config.DATA.TEST_SR_IMAGES_DIR,config.EXP.NAME), exist_ok=True)
 
     # Start the verification mode of the bsrgan_model.
-    g_model.eval()
+    generator.eval()
 
     # Initialize the sharpness evaluation function
-    psnr = PSNR(srgan_config.upscale_factor, srgan_config.only_test_y_channel)
-    ssim = SSIM(srgan_config.upscale_factor, srgan_config.only_test_y_channel)
-
-    # Set the sharpness evaluation function calculation device to the specified model
-    psnr = psnr.to(device=srgan_config.device, non_blocking=True)
-    ssim = ssim.to(device=srgan_config.device, non_blocking=True)
+    ssim_model = SSIM(config.DATA.UPSCALE_FACTOR, True).to(device=config.MODEL.DEVICE)
+    psnr_model = PSNR(config.DATA.UPSCALE_FACTOR, True).to(device=config.MODEL.DEVICE)
 
     # Initialize IQA metrics
-    psnr_metrics = 0.0
-    ssim_metrics = 0.0
+    psnr_avg = 0.0
+    ssim_avg = 0.0
 
     # Get a list of test image file names.
-    file_names = natsorted(os.listdir(srgan_config.lr_dir))
-    # Get the number of test image files.
-    total_files = len(file_names)
+    file_names = natsorted(os.listdir(config.DATA.TEST_LR_IMAGES_DIR))
+    n_images = len(file_names)
 
-    for index in range(total_files):
-        lr_image_path = os.path.join(srgan_config.lr_dir, file_names[index])
-        sr_image_path = os.path.join(srgan_config.sr_dir, file_names[index])
-        gt_image_path = os.path.join(srgan_config.gt_dir, file_names[index])
+    for im_name in file_names:
+        lr_image_path = os.path.join(config.DATA.TEST_LR_IMAGES_DIR, im_name)
+        sr_image_path = os.path.join(config.DATA.TEST_SR_IMAGES_DIR, config.EXP.NAME, im_name)
+        gt_image_path = os.path.join(config.DATA.TEST_GT_IMAGES_DIR, im_name)
 
         print(f"Processing `{os.path.abspath(lr_image_path)}`...")
-        lr_tensor = imgproc.preprocess_one_image(lr_image_path, srgan_config.device)
-        gt_tensor = imgproc.preprocess_one_image(gt_image_path, srgan_config.device)
+        lr_tensor = imgproc.preprocess_one_image(lr_image_path, config.MODEL.DEVICE)
+        gt_tensor = imgproc.preprocess_one_image(gt_image_path, config.MODEL.DEVICE)
 
         # Only reconstruct the Y channel image data.
         with torch.no_grad():
-            sr_tensor = g_model(lr_tensor)
+            sr_tensor = generator(lr_tensor)
 
         # Save image
         sr_image = imgproc.tensor_to_image(sr_tensor, False, False)
@@ -83,41 +65,23 @@ def main() -> None:
         cv2.imwrite(sr_image_path, sr_image)
 
         # Cal IQA metrics
-        psnr_metrics += psnr(sr_tensor, gt_tensor).item()
-        ssim_metrics += ssim(sr_tensor, gt_tensor).item()
+        psnr_avg += psnr_model(sr_tensor, gt_tensor).item()
+        ssim_avg += ssim_model(sr_tensor, gt_tensor).item()
 
-    # Calculate the average value of the sharpness evaluation index,
-    # and all index range values are cut according to the following values
-    # PSNR range value is 0~100
-    # SSIM range value is 0~1
-    avg_psnr = 100 if psnr_metrics / total_files > 100 else psnr_metrics / total_files
-    avg_ssim = 1 if ssim_metrics / total_files > 1 else ssim_metrics / total_files
-
+    psnr_avg /= n_images
+    ssim_avg /= n_images
 
     # Write PSNR and SSIM data to file and terminal
-    psnr_label = f"PSNR: {avg_psnr:4.2f} [dB]\n"
-    ssim_label = f"SSIM: {avg_ssim:4.4f} [u]"
+    psnr_label = f"PSNR: {psnr_avg:4.2f} [dB]\n"
+    ssim_label = f"SSIM: {ssim_avg:4.4f} [u]"
     print(psnr_label + ssim_label)
     
-    out_file = open(f"results/test/{srgan_config.exp_name}/metrics.txt","w")
+    out_file = open(f"results/test/{config.EXP.NAME}/_metrics.txt","w")
     out_file.writelines([psnr_label, ssim_label])
     out_file.close()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-                    prog = 'SRGAN-ST',
-                    description = 'Does super resolution',
-                    epilog = 'Text at the bottom of help bla bla')
-    parser.add_argument('-exp_name', type=str, help='The name of the experiment')
-    parser.add_argument('-g_weights', type=str, help='The path of the g weights')#, default="./results/pretrained_models/SRGAN_x4-ImageNet-8c4a7569.pth.tar")
-    
-    args = parser.parse_args()
-
-    srgan_config.exp_name = args.exp_name
-    srgan_config.g_model_weights_path = args.g_weights
-    srgan_config.mode = "test"
-
-    srgan_config.sr_dir = f"./results/test/{srgan_config.exp_name}"
-    
-    main()
+    config = Config()
+    g_path = 'results/warmup-exp/g_best.pth'
+    test(config = config, g_path = g_path)
