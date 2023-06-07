cfa4c86573a2e57520a11468c9e09935b7a9a99c
SebastianBitsch <sebastianbitsch@protonmail.com>
2023-06-07 08:54:58 +0200

    added comments to config

--- config.py
+++ config.py
@@ -16,46 +16,50 @@ class dotdict(dict):
 
 class Config():
 
-    DEVICE = "cuda:0"
+    DEVICE = "cuda:0"               # Device, should only really be cuda tbh
 
     EXP = dotdict()
-    EXP.USER = "s204163"
-    EXP.NAME = "experiment-name"
+    EXP.USER = "s204163"            # The user id on HPC the scratch directory in on
+    EXP.NAME = "experiment-name"    # Name of the experiment, folders etc. will be named by this
     EXP.START_EPOCH = 0             # Whether to resume training at some epoch number or start at epoch 0
     EXP.N_EPOCHS = 20               # Number of epochs to train for
     EXP.LABEL_SMOOTHING = 0.0       # One-sided label smoothing. The true label will be 1.0 - label_smoothing
 
     # Logging options
-    LOG_TRAIN_PERIOD = 100
-    LOG_VALIDATION_PERIOD = 1
+    LOG_TRAIN_PERIOD = 100          # How many iterations should be between each print loss statement when training
+    LOG_VALIDATION_PERIOD = 1       # How many iterations should be between each psnr/ssim log statement when validating
 
     # Data
     DATA = dotdict()
-    DATA.TRAIN_GT_IMAGES_DIR = f"/work3/{EXP.USER}/data/ImageNet/train" # Training HR gt images 
-    DATA.TEST_SET = 'Set5'                                          # The test set to use; Set5, Set14, BSD100, Urban100
-    DATA.TEST_GT_IMAGES_DIR = F"/work3/{EXP.USER}/data/{DATA.TEST_SET}/GTmod12"    # Test HR images
-    DATA.TEST_LR_IMAGES_DIR = f"/work3/{EXP.USER}/data/{DATA.TEST_SET}/LRbicx4"    # Test downscaled images
-    DATA.TEST_SR_IMAGES_DIR = "results/test"                       # Directory to output the SR images to in test.py
-    DATA.SEED = 1312
-    DATA.UPSCALE_FACTOR = 4
-    DATA.BATCH_SIZE = 16
-    DATA.GT_IMAGE_SIZE = 96    # Size of the HR GT images i.e. 192 x 192
+    DATA.TRAIN_GT_IMAGES_DIR = f"/work3/{EXP.USER}/data/ImageNet/train"             # Location of training HR gt images 
+    DATA.TEST_SET = 'Set5'                                                          # The test set to use; Set5, Set14, BSD100, Urban100
+    DATA.TEST_GT_IMAGES_DIR = F"/work3/{EXP.USER}/data/{DATA.TEST_SET}/GTmod12"     # Location of test HR images
+    DATA.TEST_LR_IMAGES_DIR = f"/work3/{EXP.USER}/data/{DATA.TEST_SET}/LRbicx4"     # Location of test downscaled images
+    DATA.TEST_SR_IMAGES_DIR = "results/test"                                        # Directory to output the SR images to in test.py
+    DATA.SEED = 1312            # The seed to use for reproducability
+    DATA.UPSCALE_FACTOR = 4     # The upscale factor, only really tested for 4
+    DATA.BATCH_SIZE = 16        # The batchsize of images to use
+    DATA.GT_IMAGE_SIZE = 96     # Size of the HR ground truth images i.e. 192 x 192
     
     # Model
     MODEL = dotdict()
-    MODEL.CONTINUE_FROM_WARMUP = False
-    MODEL.WARMUP_WEIGHTS = "results/Resnet-first/g_best.pth"
-    # Generator
-    MODEL.G_IN_CHANNEL = 3     
-    MODEL.G_OUT_CHANNEL = 3
-    MODEL.G_N_CHANNEL = 64
+    MODEL.CONTINUE_FROM_WARMUP = False                          # Should the generator continue from some pretrained weights?
+    MODEL.WARMUP_WEIGHTS = "results/Resnet-first/g_best.pth"    # Directory of weights to use if we continue from warmup
+    
+    # Generator network parameters
+    MODEL.G_IN_CHANNEL = 3          # In color channels
+    MODEL.G_OUT_CHANNEL = 3         # Out color channels
+    MODEL.G_N_CHANNEL = 64          # 
     MODEL.G_N_RCB = 16
     MODEL.G_LOSS = dotdict()
-    MODEL.G_LOSS.VGG19_LAYERS = {       # The layers and weights from VGG19 that are used in the content loss 
+
+    # The layers and weights from VGG19 that are used in the ContentLoss() 
+    MODEL.G_LOSS.VGG19_LAYERS = {
         "features.17" : 1/8,
         "features.26" : 1/4,
         "features.35" : 1/2
     }
+    # The layers and weights from the discriminator to use in the DiscriminatorFeaturesLoss()
     MODEL.G_LOSS.DISC_FEATURES_LOSS_LAYERS = {
         "features.5" : 1.0
     }
@@ -67,11 +71,11 @@ class Config():
     }
     # How to weigh the loss functions used in the generator
     MODEL.G_LOSS.CRITERION_WEIGHTS = {  
-        "Adversarial"   : 0.005,
+        "Adversarial"   : 0.001,
         "Content"       : 1.0,
         "Pixel"         : 1.0,
     }
-    # A list of criterions the generator should use during warmup
+    # Which criterions should the generator use during warmup. Defaults to just pixel loss
     MODEL.G_LOSS.WARMUP_CRITERIONS = {
         "Pixel"         : nn.MSELoss()
     }
@@ -102,7 +106,7 @@ class Config():
     # Scheduler
     SCHEDULER = dotdict()
     SCHEDULER.STEP_SIZE = EXP.N_EPOCHS // 2
-    SCHEDULER.GAMMA = 0.1 # or 0.5
+    SCHEDULER.GAMMA = 0.5 # or 0.5
 
 
     def add_g_criterion(self, name: str, value: nn.Module, weight: float = 1.0) -> None:
